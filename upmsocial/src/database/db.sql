b83b34bda69617396f93dcdbcc36f3cfdefc362d
Víctor Nieves Sánchez <vnievess@gmail.com>
2019-04-02 14:55:42 +0200

    changed user and pw for db

--- upmsocial/src/database/db.sql
+++ upmsocial/src/database/db.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: upmsocial
 -- ------------------------------------------------------
--- Server version	5.7.25-0ubuntu0.18.04.2
+-- Server version	5.7.25-1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -121,4 +121,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-02 14:41:24
+-- Dump completed on 2019-04-02 14:52:56
