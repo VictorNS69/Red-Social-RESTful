701bbd8ba886b221a9e7cf02ddc3d6bf616aaf50
Victor Nieves Sanchez <vnievess@gmail.com>
2019-04-16 22:06:22 +0200

    reestructured the code. Now is more beautiful

--- upmsocial/src/cliente/Main.java
+++ upmsocial/src/cliente/Main.java
@@ -18,23 +18,190 @@ import java.util.Scanner;
 
 public class Main {
 
+	private static WebTarget target;
+		
 	private static URI getBaseURI() {
         return UriBuilder.fromUri("http://localhost:8080/upmsocial/").build();
     }
+
+	@SuppressWarnings("resource")
+	private static void listaUsuarios() {
+		Response r;
+		String valor = "";
+		InterfazOperacionesC ops = new Operaciones(target);
+		r = ops.listaUsuarios();
+		System.out.println("Estado: " + r.getStatus());
+        valor = r.readEntity(String.class);
+        System.out.println("Entidad: " + valor);
+        r.close();
+	}
+
+	@SuppressWarnings("resource")
+	private static void anadirUsuario() {		
+		Response r;
+		String valor = "";
+		InterfazOperacionesC ops = new Operaciones(target);
+		Scanner in2 = new Scanner(System.in);
+		System.out.println("Escribe un nombre.");
+		String nombre = in2.nextLine();
+		System.out.println("Escribe un apellido.");
+		String apellido1 = in2.nextLine();
+		System.out.println("Escribe otro apellido.");
+		String apellido2 = in2.nextLine();
+		System.out.println("Escribe un email.");
+		String email = in2.nextLine();
+		System.out.println("Escribe un pais.");
+		String pais = in2.nextLine();
+		System.out.println("Escribe un numero de telefono.");
+		int telefono = in2.nextInt();
+		Usuario usuario = new Usuario(nombre, apellido1, 
+				apellido2, telefono, email, pais);
+		r = ops.anadirUsuario(usuario);
+		System.out.println("Estado: " + r.getStatus());
+        valor = r.readEntity(String.class);
+        System.out.println("Entidad: " + valor);
+        r.close();
+	}
+
+	@SuppressWarnings("resource")
+	private static void infoUsuario() {
+		Response r;
+		String valor = "";
+		InterfazOperacionesC ops = new Operaciones(target);
+		Scanner in3 = new Scanner(System.in);
+		System.out.println("Escribe un id.");
+		int id = in3.nextInt();
+		r = ops.infoUsuario(id);
+		System.out.println("Estado: " + r.getStatus());
+        valor = r.readEntity(String.class);
+        System.out.println("Entidad: " + valor);
+		r.close();
+	}
+
+	@SuppressWarnings("resource")
+	private static void modificaUsuario() {
+		Response r;
+		String valor = "";
+		InterfazOperacionesC ops = new Operaciones(target);
+		Scanner in4 = new Scanner(System.in);
+		System.out.println("Escribe un id.");
+		Scanner in = new Scanner(System.in);
+		int id = in.nextInt();
+		System.out.println("Escribe un nombre.");
+		String nombre = in4.nextLine();
+		System.out.println("Escribe un apellido.");
+		String apellido1 = in4.nextLine();
+		System.out.println("Escribe otro apellido.");
+		String apellido2 = in4.nextLine();
+		System.out.println("Escribe un email.");
+		String email = in4.nextLine();
+		System.out.println("Escribe un pais.");
+		String pais = in4.nextLine();
+		System.out.println("Escribe un numero de telefono.");
+		int telefono = in4.nextInt();
+		Usuario usuario = new Usuario(nombre, apellido1, apellido2, 
+				telefono, email, pais);
+		r = ops.modificaUsuario(id, usuario);
+		System.out.println("Estado: " + r.getStatus());
+        valor = r.readEntity(String.class);
+        System.out.println("Entidad: " + valor);
+		r.close();
+	}
+
+	@SuppressWarnings("resource")
+	private static void eliminaUsuario() {
+		Response r;
+		String valor = "";
+		InterfazOperacionesC ops = new Operaciones(target);
+		Scanner in5 = new Scanner(System.in);
+		System.out.println("Escribe un id.");
+		int id = in5.nextInt();
+		r = ops.eliminaUsuario(id);
+		System.out.println("Estado: " + r.getStatus());
+        valor = r.readEntity(String.class);
+        System.out.println("Entidad: " + valor);
+		r.close();
+	}
+
+	@SuppressWarnings("resource")
+	private static void amigosUsuario() {
+		Response r;
+		String valor = "";
+		InterfazOperacionesC ops = new Operaciones(target);
+		System.out.println("Escribe un id.");
+		Scanner in6 = new Scanner(System.in);
+		int id = in6.nextInt();
+		try {
+			r = ops.amigosUsuario(id);
+			System.out.println("Estado: " + r.getStatus());
+	        valor = r.readEntity(String.class);
+	        System.out.println("Entidad: " + valor);
+	        r.close();
+		} catch (NumberFormatException e) {
+			System.err.println("Opción introducida no válida.");
+		}
+	}
+
+	@SuppressWarnings("resource")
+	private static void anadirAmigoUsuario() {
+		Response r;
+		String valor = "";
+		InterfazOperacionesC ops = new Operaciones(target);
+		Scanner in7 = new Scanner(System.in);
+		System.out.println("Escribe el id del usuario.");
+		int idU = in7.nextInt();
+		System.out.println("Escribe el id del amigo.");
+		int idA = in7.nextInt();
+		r = ops.anadirAmigoUsuario(idU, idA);
+		System.out.println("Estado: " + r.getStatus());
+        valor = r.readEntity(String.class);
+        System.out.println("Entidad: " + valor);
+        r.close();
+	}
+
+	@SuppressWarnings("resource")
+	private static void borrarAmigoUsuario() {
+		Response r;
+		String valor = "";
+		InterfazOperacionesC ops = new Operaciones(target);
+		Scanner in8 = new Scanner(System.in);
+		System.out.println("Escribe el id del usuario.");
+		int idU = in8.nextInt();
+		System.out.println("Escribe el id del amigo.");
+		int idA = in8.nextInt();
+		r = ops.borrarAmigoUsuario(idU, idA);
+		System.out.println("Estado: " + r.getStatus());
+        valor = r.readEntity(String.class);
+        System.out.println("Entidad: " + valor);
+		r.close();
+	}
+
+	@SuppressWarnings("resource")
+	private static void mensajesMuroUsuario() {
+		Response r;
+		String valor = "";
+		InterfazOperacionesC ops = new Operaciones(target);
+		System.out.println("Escribe un id.");
+		Scanner in9 = new Scanner(System.in);
+		int id = in9.nextInt();
+		r = ops.mensajesMuroUsuario(id);
+		System.out.println("Estado: " + r.getStatus());
+        valor = r.readEntity(String.class);
+        System.out.println("Entidad: " + valor);
+        r.close();
+	}
 	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		System.out.println("Bienvenido al cliente.");
-		WebTarget target = null;
 		try {
 			ClientConfig config = new ClientConfig();
 	        Client client = ClientBuilder.newClient(config);
-	        target = client.target(getBaseURI());
+	        target = (client.target(getBaseURI()));
 		} catch (Exception e) {
 			System.err.println("No se ha podido conectar al servidor.\n Saliendo.");
 			System.exit(1);
 		}
-		InterfazOperacionesC ops = new Operaciones(target);
 		int option = -1;
 		while (true) {
 			System.out.print("\n");
@@ -53,129 +220,34 @@ public class Main {
 			try {
 				Scanner in = new Scanner(System.in);
 				option = in.nextInt();
-				Response r;
-				String valor = "";
+				
 				switch (option) {
 					case 1:
-						r = ops.listaUsuarios();
-						System.out.println("Estado: " + r.getStatus());
-				        valor = r.readEntity(String.class);
-				        System.out.println("Entidad: " + valor);
-				        r.close();
+						listaUsuarios();
 						break;
 					case 2:
-						Scanner in2 = new Scanner(System.in);
-						System.out.println("Escribe un nombre.");
-						String nombre = in2.nextLine();
-						System.out.println("Escribe un apellido.");
-						String apellido1 = in2.nextLine();
-						System.out.println("Escribe otro apellido.");
-						String apellido2 = in2.nextLine();
-						System.out.println("Escribe un email.");
-						String email = in2.nextLine();
-						System.out.println("Escribe un pais.");
-						String pais = in2.nextLine();
-						System.out.println("Escribe un numero de telefono.");
-						int telefono = in2.nextInt();
-						Usuario usuario = new Usuario(nombre, apellido1, 
-								apellido2, telefono, email, pais);
-						r = ops.anadirUsuario(usuario);
-						System.out.println("Estado: " + r.getStatus());
-				        valor = r.readEntity(String.class);
-				        System.out.println("Entidad: " + valor);
-				        r.close();
+						anadirUsuario();
 						break;
 					case 3:
-						Scanner in3 = new Scanner(System.in);
-						System.out.println("Escribe un id.");
-						int id = in3.nextInt();
-						r = ops.infoUsuario(id);
-						System.out.println("Estado: " + r.getStatus());
-				        valor = r.readEntity(String.class);
-				        System.out.println("Entidad: " + valor);
-						r.close();
+						infoUsuario();
 						break;
 					case 4:
-						Scanner in4 = new Scanner(System.in);
-						System.out.println("Escribe un id.");
-						id = in.nextInt();
-						System.out.println("Escribe un nombre.");
-						nombre = in4.nextLine();
-						System.out.println("Escribe un apellido.");
-						apellido1 = in4.nextLine();
-						System.out.println("Escribe otro apellido.");
-						apellido2 = in4.nextLine();
-						System.out.println("Escribe un email.");
-						email = in4.nextLine();
-						System.out.println("Escribe un pais.");
-						pais = in4.nextLine();
-						System.out.println("Escribe un numero de telefono.");
-						telefono = in4.nextInt();
-						usuario = new Usuario(nombre, apellido1, apellido2, 
-								telefono, email, pais);
-						r = ops.modificaUsuario(id, usuario);
-						System.out.println("Estado: " + r.getStatus());
-				        valor = r.readEntity(String.class);
-				        System.out.println("Entidad: " + valor);
-						r.close();
+						modificaUsuario();
 						break;
 					case 5:
-						Scanner in5 = new Scanner(System.in);
-						System.out.println("Escribe un id.");
-						id = in5.nextInt();
-						r = ops.eliminaUsuario(id);
-						System.out.println("Estado: " + r.getStatus());
-				        valor = r.readEntity(String.class);
-				        System.out.println("Entidad: " + valor);
-						r.close();
+						eliminaUsuario();
 						break;
 					case 6:
-						System.out.println("Escribe un id.");
-						Scanner in6 = new Scanner(System.in);
-						id = in6.nextInt();
-						try {
-							r = ops.amigosUsuario(id);
-							System.out.println("Estado: " + r.getStatus());
-					        valor = r.readEntity(String.class);
-					        System.out.println("Entidad: " + valor);
-					        r.close();
-						} catch (NumberFormatException e) {
-							System.err.println("Opción introducida no válida.");
-						}
+						amigosUsuario();
 						break;
 					case 7:
-						Scanner in7 = new Scanner(System.in);
-						System.out.println("Escribe el id del usuario.");
-						int idU = in7.nextInt();
-						System.out.println("Escribe el id del amigo.");
-						int idA = in7.nextInt();
-						r = ops.anadirAmigoUsuario(idU, idA);
-						System.out.println("Estado: " + r.getStatus());
-				        valor = r.readEntity(String.class);
-				        System.out.println("Entidad: " + valor);
-				        r.close();
+						anadirAmigoUsuario();
 						break;
 					case 8:
-						Scanner in8 = new Scanner(System.in);
-						System.out.println("Escribe el id del usuario.");
-						idU = in8.nextInt();
-						System.out.println("Escribe el id del amigo.");
-						idA = in8.nextInt();
-						r = ops.borrarAmigoUsuario(idU, idA);
-						System.out.println("Estado: " + r.getStatus());
-				        valor = r.readEntity(String.class);
-				        System.out.println("Entidad: " + valor);
-						r.close();
+						borrarAmigoUsuario();
 						break;
 					case 9:
-						System.out.println("Escribe un id.");
-						Scanner in9 = new Scanner(System.in);
-						id = in9.nextInt();
-						r = ops.mensajesMuroUsuario(id);
-						System.out.println("Estado: " + r.getStatus());
-				        valor = r.readEntity(String.class);
-				        System.out.println("Entidad: " + valor);
-				        r.close();
+						mensajesMuroUsuario();
 				        break;
 					// TODO: Continuar
 					case 69:
